c5e89740acb96105dc209c0191c646ab487b2149
karenxiong <karen.xiong@mail.utoronto.ca>
2016-11-11 23:16:20 -0500

    added project wrapped in react + material UI box

---
+++ src/app/Main.js
@@ -0,0 +1,23 @@
+/**
+ * In this file, we create a React component
+ * which incorporates components provided by Material-UI.
+ */
+import React, {Component} from 'react';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div>hisdklfnsdklfdslkfmdsklmdlksmlkds</div>
+    );
+  }
+}
+
+export default Main;
